8403d622002411fd07e1462909957efc7ae2373a
kyelShades <ntiamoahachiaairene729>
2024-02-05 10:01:14 +0000

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

--- showout/views.py
+++ showout/views.py
@@ -19,6 +19,37 @@ def cart(request):
     context ={}
     return render (request, 'showout/cart.html', context)
 
+def checkout(request):
+    context ={}
+    return render (request, 'showout/checkout.html', context)
+
+
+def orderDetails(request):
+    context ={}
+    return render (request, 'showout/orderDetails.html', context)
+
+def orders(request):
+    context ={}
+    return render (request, 'showout/orders.html', context)
+
+def productDetails(request):
+    context = {}
+    return render (request, 'showout/productDetails.html', context)
+
+def profile(request):
+    context = {}
+    return render (request, 'showout/profile.html', context)
+
+
+    context = {}
+    return render (request, 'showout/report.html', context)
+def resetPassword(request):
+    context = {}
+    return render (request, 'showout/resetPassword.html', context)
+
+
+
+
 
 
 # add the rest of the view for the various webpages
