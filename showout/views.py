a8ff95f27f25287f05eb40fca6178c9051c2f5ed
Sylensa <sylensa.adolf@gmail.com>
2024-02-12 11:47:34 +0000

    folder chnages

--- showout/views.py
+++ showout/views.py
@@ -5,91 +5,88 @@ from django.shortcuts import render
 
 def login(request):
     context = {}
-    return render(request, 'showout/login.html', context)
+    return render(request, 'showout/customers/login.html', context)
 
 def signup(request):
     context = {}
-    return render(request, 'showout/signup.html', context)
+    return render(request, 'showout/customers/signup.html', context)
 
 def home(request):
     context = {}
-    return render(request,'showout/home.html', context)
+    return render(request,'showout/customers/home.html', context)
 
 def cart(request):
     context ={}
-    return render (request, 'showout/cart.html', context)
+    return render (request, 'showout/customers/cart.html', context)
 
 def checkout(request):
     context ={}
-    return render (request, 'showout/checkout.html', context)
+    return render (request, 'showout/customers/checkout.html', context)
 
 
 def orderDetails(request):
     context ={}
-    return render (request, 'showout/orderDetails.html', context)
+    return render (request, 'showout/customers/orderDetails.html', context)
 
 def orders(request):
     context ={}
-    return render (request, 'showout/orders.html', context)
+    return render (request, 'showout/customers/orders.html', context)
 
 def productDetails(request):
     context = {}
-    return render (request, 'showout/productDetails.html', context)
+    return render (request, 'showout/customers/productDetails.html', context)
 
 def profile(request):
     context = {}
-    return render (request, 'showout/profile.html', context)
+    return render (request, 'showout/customers/profile.html', context)
 
-
-    context = {}
-    return render (request, 'showout/report.html', context)
 def resetPassword(request):
     context = {}
-    return render (request, 'showout/resetPassword.html', context)
+    return render (request, 'showout/customers/resetPassword.html', context)
 
 def register(request):
     context = {}
-    return render (request, 'showout/register.html', context)
+    return render (request, 'showout/customers/register.html', context)
 
 def changePassword(request):
     context = {}
-    return render (request, 'showout/changePassword.html', context)
+    return render (request, 'showout/customers/changePassword.html', context)
 
 def vendorPage(request):
     context = {}
-    return render (request, 'showout/vendorPage.html', context)
+    return render (request, 'showout/customers/vendorPage.html', context)
 
 def servicePage(request):
     context = {}
-    return render (request, 'showout/servicePage.html', context)
+    return render (request, 'showout/customers/servicePage.html', context)
 
 def viewServices(request):
     context = {}
-    return render (request, 'showout/viewServices.html', context)
+    return render (request, 'showout/customers/viewServices.html', context)
 
 def viewVendors(request):
     context = {}
-    return render (request, 'showout/viewVendors.html', context)
+    return render (request, 'showout/customers/viewVendors.html', context)
 
 def wishlist(request):
     context = {}
-    return render (request, 'showout/wishlist.html', context)
+    return render (request, 'showout/customers/wishlist.html', context)
 
 def editProfile(request):
     context = {}
-    return render (request, 'showout/editProfile.html', context)
+    return render (request, 'showout/customers/editProfile.html', context)
 
 def aboutUS(request):
     context = {}
-    return render (request, 'showout/aboutUS.html', context)
+    return render (request, 'showout/customers/aboutUS.html', context)
 
 def contactUS(request):
     context = {}
-    return render (request, 'showout/contactUS.html', context)
+    return render (request, 'showout/customers/contactUS.html', context)
 
 def searchResult(request):
     context = {}
-    return render (request, 'showout/searchResult.html', context)
+    return render (request, 'showout/customers/searchResult.html', context)
 
 
 
